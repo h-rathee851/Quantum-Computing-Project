0d23e35fce88f51eed7e64a6e1ce365698b9c522
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2019-02-21 00:40:26 +0000

    Many many more gates.

--- src/operators.py
+++ src/operators.py
@@ -6,7 +6,22 @@ from sparse_matrix import SparseMatrix
 from quantum_operator import Operator  # Couldn't just be called operator as operator already exists.
 
 
-class Hadamard(Operator):
+class I(Operator):
+    """
+    Identity operator. Created an identity matrix of size 2*n_qubits and sets
+    each element on the major diagonal to 1 while leving all other elements 0.
+    """
+    def __init__(self, n_qubits: int=1):
+        dimension = 2*n_qubits
+        base = np.zeros((dimension, dimension))
+        for i in range(dimension):
+            for j in range(dimension):
+                if i == j:
+                    base[i][j] = 1
+        super(I, self).__init__(n_qubits, base)
+
+
+class H(Operator):
     """
     Hadamard gate acts on a single qubit. This gate maps the state,
     |0> to (|0> + |1>) / sqrt(2) and |1> to (|0> - |1>) / sqrt(2), creating a
@@ -17,21 +32,10 @@ class Hadamard(Operator):
         # Define "base" hadamard matrix for one qubit and correponding sparse matrix
         base = 1 / np.sqrt(2) * np.array([[1, 1],
                                         [1, -1]])
-        super(Hadamard, self).__init__(n_qubits, base)
+        super(H, self).__init__(n_qubits, base)
 
-class CNot(Operator):
-    """
-    Controlled not gate (or cX gate) which acts on 2 qubits and performs the not
-    operation on the second qubit only when the first (control) qubit is |1>.
-    """
-    def __init__(self, n_qubits: int=2):  
-        base = np.array([[1, 0, 0, 0],
-                       [0, 1, 0, 0],
-                       [0, 0, 0, 1],
-                       [0, 0, 1, 0]])
-        super(CNot, self).__init__(n_qubits, base)
 
-class XGate(Operator):
+class X(Operator):
     """
     Pauli-X gate acts on a single qubit. It is the quantum equivelent of the
     classical NOT gate. This is equivelent to a rotaation of the Bloch sphere
@@ -40,16 +44,29 @@ class XGate(Operator):
     def __init__(self, n_qubits: int=1):
         base = np.array([[0, 1],
                         [1, 0]])
-        super(XGate, self).__init__(n_qubits, base)
+        super(X, self).__init__(n_qubits, base)
 
-class ZGate(Operator):
+
+class Y(Operator):
+    """
+    The Pauli-Y gate acts on a single qubit. It equates to a rotation around the
+    Y-axis of the Bloch sphere by pi radians.
+    """
+    def __init__(self, n_qubits: int=1):  # Check if n_qubits should be 2.
+        base = np.array([[0, 1j],
+                        [1j, 0]])
+        super(Y, self).__init__(n_qubits, base)
+
+
+class Z(Operator):
     """
     Pauli-Z gate acts on a single qubit. It is equivelent to a rotation about
     the Z-axis of the Bloch sphere by pi radians.
     """
     def __init__(self, n_qubits: int=1):
         base = np.diag([1, -1])
-        super(ZGate, self).__init__(n_qubits, base)
+        super(Z, self).__init__(n_qubits, base)
+
 
 class R_phi(Operator):
     """
@@ -60,46 +77,46 @@ class R_phi(Operator):
         base = np.diag([1, np.exp(1j * phi)])
         super(R_phi, self).__init__(n_qubits, base)
 
-class CVGate(Operator):
-    def __init__(self, n_qubits: int=2):  # Check if n_qubits should be 2.
-        base = np.diag([1, 1, 1, 1j])
-        super(CVGate, self).__init__(n_qubits, base)
 
-class YGate(Operator):
-    """
-    The Pauli-Y gate acts on a single qubit. It equates to a rotation around the
-    Y-axis of the Bloch sphere by pi radians.
-    """
-    def __init__(self, n_qubits: int=1):  # Check if n_qubits should be 2.
-        base = np.array([[0, 1j],
-                        [1j, 0]])
-        super(YGate, self).__init__(n_qubits, base)
+class RX(Operator):
+    def __init__(self, phi, n_qubits: int=1):
+        base = np.array([[np.cos(phi / 2.), -1j * np.sin(phi / 2.)],
+                        [-1j * np.sin(phi / 2.), np.cos(phi / 2.)]])
+        super(RX, self).__init__(n_qubits, base)
 
-class Swap(Operator):
+
+class RY(Operator):
+    def __init__(self, phi, n_qubits: int=1):
+        base = np.array([[np.cos(phi / 2.), -np.sin(phi / 2.)],
+                        [np.sin(phi / 2.), np.cos(phi / 2.)]])
+        super(RY, self).__init__(n_qubits, base)
+
+
+class RZ(Operator):
+    def __init__(self, phi, n_qubits: int=1):
+        base = np.array([[np.cos(phi / 2.) -1j * np.sin(phi / 2.), 0],
+                        [0, -1j * np.sin(phi / 2.) + np.cos(phi / 2.)]])
+        super(RZ, self).__init__(n_qubits, base)
+
+
+class CZ(Operator):
+    def __init__(self, n_qubits: int=2):
+        base = np.diag([1, 1, 1, -1])
+        super(CZ, self).__init__(n_qubits, base)
+
+
+class CNOT(Operator):
     """
-    The Swap gate acts on two qubits and swaps the states of the two input
-    qubits.
-    Example: |10> -> |01> or |01> -> |10>.
+    Controlled not gate (or cX gate) which acts on 2 qubits and performs the not
+    operation on the second qubit only when the first (control) qubit is |1>.
     """
-    def __init__(self, n_qubits: int=2):  # Check if n_qubits should be 2.
+    def __init__(self, n_qubits: int=2):
         base = np.array([[1, 0, 0, 0],
-                        [0, 0, 1, 0],
-                        [0, 1, 0, 0],
-                        [0, 0, 0, 1]])
-        super(Swap, self).__init__(n_qubits, base)
+                       [0, 1, 0, 0],
+                       [0, 0, 0, 1],
+                       [0, 0, 1, 0]])
+        super(CNOT, self).__init__(n_qubits, base)
 
-class SQUSwap(Operator):
-    """
-    The SQUSwap gate acts on two qubits and performs a half swap between the
-    two qubits.
-    qubits.
-    """
-    def __init__(self, n_qubits: int=2):  # Check if n_qubits should be 2.
-        base = np.array([[1, 0, 0, 0],
-                        [0, (1/2)*(1+1j), (1/2)*(1-1j), 0],
-                        [0, (1/2)*(1-1j), (1/2)*(1+1j), 0],
-                        [0, 0, 0, 1]])
-        super(SQUSwap, self).__init__(n_qubits, base)
 
 class CCNOT(Operator):
     """
@@ -116,6 +133,49 @@ class CCNOT(Operator):
                         [0, 0, 0, 0, 0, 0, 0, 1],
                         [0, 0, 0, 0, 0, 0, 1, 0]])
         super(CCNOT, self).__init__(n_qubits, base)
+
+
+class S(Operator):
+    def __init__(self, n_qubits: int=1):
+        base = np.diag([1, 1j])
+        super(S, self).__init__(n_qubits, base)
+
+
+class T(Operator):
+    def __init__(self, n_qubits: int=1):
+        base = np.diag([1, cmath.exp(1j * np.pi / 4.0)])
+        super(T, self).__init__(n_qubits, base)
+
+
+
+class SWAP(Operator):
+    """
+    The Swap gate acts on two qubits and swaps the states of the two input
+    qubits.
+    Example: |10> -> |01> or |01> -> |10>.
+    """
+    def __init__(self, n_qubits: int=2):  # Check if n_qubits should be 2.
+        base = np.array([[1, 0, 0, 0],
+                        [0, 0, 1, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 0, 1]])
+        super(SWAP, self).__init__(n_qubits, base)
+
+
+class SQUSwap(Operator):
+    """
+    The SQUSwap gate acts on two qubits and performs a half swap between the
+    two qubits.
+    qubits.
+    """
+    def __init__(self, n_qubits: int=2):  # Check if n_qubits should be 2.
+        base = np.array([[1, 0, 0, 0],
+                        [0, (1/2)*(1+1j), (1/2)*(1-1j), 0],
+                        [0, (1/2)*(1-1j), (1/2)*(1+1j), 0],
+                        [0, 0, 0, 1]])
+        super(SQUSwap, self).__init__(n_qubits, base)
+
+
 
 class CSWAP(Operator):
     """
@@ -134,19 +194,27 @@ class CSWAP(Operator):
                         [0, 0, 0, 0, 0, 0, 0, 1]])
         super(CSWAP, self).__init__(n_qubits, base)
 
-class Identity(Operator):
-    """
-    Identity operator. Created an identity matrix of size 2*n_qubits and sets
-    each element on the major diagonal to 1 while leving all other elements 0.
-    """
-    def __init__(self, n_qubits: int=1):
-        dimension = 2*n_qubits
-        base = np.zeros((dimension, dimension))
-        for i in range(dimension):
-            for j in range(dimension):
-                if i == j:
-                    base[i][j] = 1
-        super(Identity, self).__init__(n_qubits, base)
+
+class ISWAP(Operator):
+    def __init__(self, n_qubits: int=2):
+        base = np.array([1, 0, 0, 0],
+                        [0, 0, 1j, 0],
+                        [0, 1j, 0, 0],
+                        [0, 0, 0, 1])
+        super(ISWAP, self).__init__(n_qubits, base)
+
+
+class PSWAP(Operator):
+    def __init__(self, n_qubits: int=2):
+        base = np.array([1, 0, 0, 0],
+                        [0, 0, np.exp(1j * phi), 0],
+                        [0, np.exp(1j * phi), 0, 0],
+                        [0, 0, 0, 1])
+        super(PSWAP, self).__init__(n_qubits, base)
+
+
+
+
 
 
 def test():
