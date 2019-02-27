accd3d440c45913a82aedf9d11f369975649784c
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2019-02-27 17:11:05 +0000

    ImportError: No module named 'sparse_martix'

--- grovers_algorithm.py
+++ grovers_algorithm.py
@@ -8,3 +8,13 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from src.quantum_register import QuantumRegister
+from src.quantum_operator import Operator
+from src.sparse_matrix import SparseMatrix
+from src.operators import H, I
+
+
+a = QuantumRegister(2)
+i = I()
+print(i)
+print(a)
