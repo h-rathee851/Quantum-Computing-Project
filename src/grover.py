eda84e7c0af4f1a66bf8fd2d1b8fed82f36bb2f6
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2019-02-27 18:45:22 +0000

    Potentially done grovers just need an oricle builder (Mainly coppied).

--- src/grover.py
+++ src/grover.py
@@ -14,3 +14,55 @@ from src.sparse_matrix import SparseMatrix
 from src.quantum_register import QuantumRegister
 from src.quantum_operator import Operator
 from src.operators import *
+
+
+class Grover:
+    def __init__(self, n_qubits, target_state):
+        self.n_qubits = n_qubits
+        self.target_state = target_state
+        self.qr = None
+        self.D = None
+        self.oricle = None
+
+    def build_quantum_register(self):
+        self.qr = QuantumRegister(self.n_qubits)
+        return self.qr
+
+    def print_register(self):
+        print(self.qr)
+
+    def init_register(self):
+        h = H(self.n_qubits)
+        x = X(self.n_qubits)
+        h2 = H(self.n_qubits)
+        empty_register = QuantumRegister(self.n_qubits)
+        self.qr = h * self.qr
+        aux = h * x * empty_register
+        self.qr = self.qr * aux
+        return self.qr
+
+    def init_reflection_matrix(self):
+        h = H(self.n_qubits)
+        i = I(self.n_qubits)
+        x = X(self.n_qubits)
+        cnot = CNOT(2*self.n_qubits)
+        a = (h % i)
+        b = (x % i)
+        c = cnot
+        d = (x % i)
+        e = (h % i)
+        self.D = a * b * c * d * e
+        return self.D
+
+    def gen_oricle(self):
+        self.oricle = Oricle(self.target_state)
+        return self.oricle
+
+    def run(self, k):
+        # k is the number of tagged states
+        runs = round( ((math.pi / 4) / math.sqrt(k)) * 2**(self.n_qubits / 2))
+        for i in range(runs):
+            self.qr = self.oricle * self.qr
+            self.qr = self.D * self.qr
+        result = self.qr.measure()
+        return result
