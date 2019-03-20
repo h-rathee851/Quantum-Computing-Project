16d5146f7aa1fb085130157d1adb4d4e62958d02
A-Sobieska <47164147+A-Sobieska@users.noreply.github.com>
2019-03-20 18:56:30 +0000

    Update Shors_algorithm.py

--- src/Shors_algorithm.py
+++ src/Shors_algorithm.py
@@ -49,8 +49,6 @@ def continued_fraction(y, Q):
 
     return a
 
-
-
 def all_Shor(N, t_qubits):
     """
     Main function to carry the full simulation of Shor's algorithm.
@@ -60,84 +58,86 @@ def all_Shor(N, t_qubits):
 
     """
     l = 0 #when l == 1, then the while loop is stopped - factors are found;
-    #when l == 2, quantum period-finding is not required
+    repeat = 0 #for checking if the quantum subroutine was repeated
+    
     while l == 0: #when no non-trivial factor of N is found
         #classical preprocessing
-        m = random.randint(1, N-1) #pick a random integer m, such that  1 < m < N
-        Q = 2**t_qubits #number of superposition states, it holds for inequality N^2 =< Q < 2N^2
+        m = random.randint(2, N-1) #pick a random integer m, such that  1 < m < N
+        Q = 2**t_qubits
         b = math.gcd(m, N) #find the greatest common divisor of m and N
 
         if b != 1:
             #b is a nontrivial factor of N
             l = 2
-            f = N / b
+            f = N/b
             print("No need to proceed to phase estimation.")
             print(str(b) + " and " + str(int(f)) + " are non-trivial factors of " + str(N) + ".")
+
         else: #if b == 1
             #Quantum part
             #do period-finding using phase estimation
+            if repeat == 0:
+                #do it only once at the first time quantum measurement is performed
+                inv_qft = invQFT(t_qubits) #initialising inverse QFT
+
             QR3, second_reg_vals_ = UaGate(N, m, t_qubits)
             QR = measure_second_reg(N, m, t_qubits, second_reg_vals_)
+            ft = inv_qft * QR #apply inverse quantum Fourier transform
+            ft.plotRegister() #show a plot of amplitudes for each qubit state 
 
-            ft = invQFT(t_qubits) * QR #apply inverse quantum Fourier transform
-            #show a plot of possibilities for each qubit state to be measured
-            ft.plotRegister()
-
-            print("measurement on the first register")
             y = ft.measure()
+            print("Measurement on the first register.")
             print(y)
-
             #classical postprocessing
             #Estimate y/Q in lower terms using continued fraction expansion. This will yield d/r estimation.
-            #r is the period
 
-            if y != 0: #if the measurement from the first register is not 0
-                try:
-                    a = continued_fraction(y, Q)
+            if y != 0:
+                #continued fraction expansion
+                a = continued_fraction(y, Q)
 
-                    #find all candidates for integer d
-                    d = []
-                    #print(a)
-                    d.append(a[0]) #d0 = a0
-                    d.append(1 + a[0]*a[1]) #d1 = 1 + a0*a1
-                    for i in range(2, len(a)):
-                        d.append(a[i]*d[i-1]+d[i-2])
+                #find all candidates for integer d
+                d = []
+                d.append(a[0]) #d0 = a0
+                d.append(1 + a[0]*a[1]) #d1 = 1 + a0*a1
+                for i in range(2, len(a)):
+                    d.append(a[i]*d[i-1]+d[i-2])
 
-                    #find all possible candidates for period r
-                    period = []
-                    period.append(1)
-                    period.append(a[1])
-                    for i in range(2, len(a)):
-                        period.append(a[i]*period[i-1]+period[i-2])
+                #find all possible candidates for period r
+                period = []
+                period.append(1)
+                period.append(a[1])
+                for i in range(2, len(a)):
+                    period.append(a[i]*period[i-1]+period[i-2])
 
-                    #finding more suitable r candidates
-                    canditate_no = []
-                    for i in range(1, len(period)):
-                        #r and d have to be co-prime
-                        div = period[i]/d[i]
-                        if math.gcd(period[i], d[i]) == 1 and div != 0 and div != 1:
-                            canditate_no.append(period[i])
+                #finding more suitable r candidates
+                canditate_no = []
+                for i in range(1, len(period)):
+                    #r and d have to be co-prime
+                    div = period[i]/d[i]
+                    if math.gcd(period[i], d[i]) == 1 and div != 0 and div != 1:
+                        canditate_no.append(period[i])
 
-                    if canditate_no: #if the list of r candidates is not empty
+                if canditate_no: #if the list of r candidates is not empty
                     #testing r candidates
-                        for k in range(len(canditate_no)):
-                            r = canditate_no[k]
-                            if r % 2 == 0 and (m**(r/2)) % N != -1: #good candidate
-                                l = 1 #stop the big while loop; factors can be found now
-                                break
-                    else: #if no r candidates were found
-                        l = 0
-                        print("No suitable r candidates were found. The algorithm is rerun.")
+                    for k in range(len(canditate_no)):
+                        r = canditate_no[k]
+                        if r % 2 == 0 and (m**(r/2)) % N != -1: #good candidate
+                            l = 1 #stop the big while loop; factors can be found now
+                            break
 
-                except:
-                    print("Continued fraction expansion cannot be calculated.")
+                else: #if no r candidates were found
                     l = 0
+                    repeat = 1 #quantum measurement might be potentially repeated
+                    print("No suitable r candidates were found. The algorithm is rerun.")
+
 
             else: #the measurement y is 0
                 l = 0
+                repeat = 1 #quantum measurement might be potentially repeated
                 print("The measurement y is 0. The algorithm is rerun.")
 
     if l == 1: #final stage: finding non-trivial factors
+
         mod = (m**(r/2) % N) + 1
         gcd = math.gcd(int(mod), N)
 
